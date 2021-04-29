5f480d515a5b8f9641e059a93d6823aacf96f533
Tomofumi Kitano <tomofumikitano@users.noreply.github.com>
2021-04-29 15:20:48 +0900

    Implemented Dark Theme

--- app/feeds/static/feeds/js/script.js
+++ app/feeds/static/feeds/js/script.js
@@ -1,15 +1,35 @@
 // https://bulma.io/documentation/components/navbar/#navbar-menu
 $(document).ready(function() {
-
-  // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
-
-
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-
-  });
+	// Check for click events on the navbar burger icon
+	$(".navbar-burger").click(function() {
+		// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+		$(".navbar-burger").toggleClass("is-active");
+		$(".navbar-menu").toggleClass("is-active");
+	});
 });
 
-console.log('Loaded script.js');
+
+// Toggle switch for Dark Theme
+// https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+	if (e.target.checked) {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		localStorage.setItem('theme', 'dark');
+	}
+	else {
+		document.documentElement.setAttribute('data-theme', 'light');
+		localStorage.setItem('theme', 'light');
+	}
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+if (currentTheme) {
+	document.documentElement.setAttribute('data-theme', currentTheme);
+	if (currentTheme === 'dark') {
+		toggleSwitch.checked = true;
+	}
+}

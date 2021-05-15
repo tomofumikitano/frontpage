d468dcaa40ec0162523781dc1a8c0e006ec76d72
Tomofumi Kitano <tomofumikitano@users.noreply.github.com>
2021-05-15 10:30:03 +0900

    Disabled DEBUG option for production

--- app/frontpage/settings.py
+++ app/frontpage/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = "*"
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1', gethostname(), gethostbyname(gethostname()), ]

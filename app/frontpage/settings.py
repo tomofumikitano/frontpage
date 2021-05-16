100e4c87e84722b47e7e51c964356a58bfd56521
Tomofumi Kitano <tomofumikitano@users.noreply.github.com>
2021-05-16 16:12:03 +0900

    Added WhiteNoise to serve static files

--- app/frontpage/settings.py
+++ app/frontpage/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,10 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
+# STATIC_ROOT = os.path.join(os.getcwd(), 'feeds', 'static', 'feeds')
+# print(STATIC_ROOT)
+
 
 # https://github.com/jcass77/django-apscheduler
 

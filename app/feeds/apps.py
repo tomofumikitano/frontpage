17cd3cbf67bfccef482cf3b082c7d3e0512f81e3
Tomofumi Kitano <tomofumikitano@users.noreply.github.com>
2021-05-18 17:27:57 +0900

    Replaced bulma directory with git submodule

--- app/feeds/apps.py
+++ app/feeds/apps.py
@@ -23,7 +23,7 @@ def start_scheduler():
 
     scheduler.add_job(
         update_all_feeds,
-        trigger=CronTrigger(hour="*", minute="*/10"),
+        trigger=CronTrigger(hour="*", minute="*/5"),
         id="update_all_feeds",  # The `id` assigned to each job MUST be unique
         max_instances=1,
         replace_existing=True,

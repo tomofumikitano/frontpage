b0b265ced7e277f7eb3a05b8fdc3401cec531da7
Tomofumi Kitano <tomofumikitano@users.noreply.github.com>
2021-05-21 21:46:16 +0900

    Fix UI

--- app/feeds/static/feeds/js/script.js
+++ app/feeds/static/feeds/js/script.js
@@ -15,10 +15,10 @@ const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"
 
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
+    document.documentElement.setAttribute('theme', 'dark');
     localStorage.setItem('theme', 'dark');
   } else {
-    document.documentElement.setAttribute('data-theme', 'light');
+    document.documentElement.setAttribute('theme', 'light');
     localStorage.setItem('theme', 'light');
   }
 }
@@ -27,7 +27,7 @@ toggleSwitch.addEventListener('change', switchTheme, false);
 
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme);
+  document.documentElement.setAttribute('theme', currentTheme);
   if (currentTheme === 'dark') {
     toggleSwitch.checked = true;
   }

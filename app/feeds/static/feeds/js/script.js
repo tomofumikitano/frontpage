f1bce7b37f7536c3a3e2569ca6679145962a759f
Tomofumi Kitano <tomofumikitano@users.noreply.github.com>
2021-09-26 22:12:20 +0900

    Dismiss notification

--- app/feeds/static/feeds/js/script.js
+++ app/feeds/static/feeds/js/script.js
@@ -6,6 +6,10 @@ $(document).ready(function() {
     $('.navbar-burger').toggleClass('is-active');
     $('.navbar-menu').toggleClass('is-active');
   });
+
+	$('.notification .delete').click(function() {
+		$(this).parent().remove();
+	});
 });
 
 
